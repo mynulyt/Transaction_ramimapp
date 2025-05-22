854e9967814275812394b7900d7945c9da89e815
Mynul Alam <mynulalam27@gmail.com>
2025-05-23 01:44:45 +0600

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: SplashScreen(),
       debugShowCheckedModeBanner: false,
     );
